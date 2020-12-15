f994a682dd852421dfa52113f408311b21c17765
ryan m <contact@ryanmeek.com>
2020-12-15 20:22:12 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,12 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
+
+  # New Code!!
+  before_save :make_title_case
 
   private
-
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
@@ -12,6 +14,7 @@ class Post < ActiveRecord::Base
   end
 
   def make_title_case
+    # Rails provides a String#titlecase method
     self.title = self.title.titlecase
   end
 end
